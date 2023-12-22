9233ca1b1e1eddeb069e69781347086eb3ca2e53
xzythu <1159108732@qq.com>
2023-12-23 00:33:23 +0800

    test

--- test/simple.c
+++ test/simple.c
@@ -1,34 +1,14 @@
-// int main()
-// {
-//     int a = 1;
-//     if (a == 1) {
-//         a = 2;
-//     } else {
-//         a = 3;
-//     }
-//     return a;
-// }
-
-int add(int a, int b){
-    return a + b;
-}
-
-
-int main (){
-    int a = 0;
-    while (a < 10)
-    {
-        int a = 2;
-        int b = 3;
-        int c;
-        int d =c+a+b;
-        {
-            int a = 3;
-            int e = c+a+b;
+int main()
+{
+    int a = 1;
+    if (a == 1) {
+        a = 2;
+    } else {
+        if (a == 3) {
+            a = 3;
+        } else {
+            a = 4;
         }
-    }
-    while (1){
-        break;
     }
     return a;
 }
